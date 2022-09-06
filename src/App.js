4548322bc825e8923d45788573ce878030db9f90
voller <voller.silas@gmail.com>
2022-09-06 13:19:18 -0300

    feat: GlobalStyle , Register and login page completed

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import GlobalStyle from "./GlobalStyle.js";
+
+import LoginPage from "./Components/LoginPage.js";
+import RegistrationPage from "./Components/RegistrationPage.js";
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <LoginPage />
+    </>
+  );
+};
+
+export default App;
